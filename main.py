35919efd2884d1e0e3f069a3b49118e92a30102d
lfercer <146728047+lfercer@users.noreply.github.com>
2023-10-02 14:01:38 +0000

    changing result in line 115 and in line 118

--- main.py
+++ main.py
@@ -112,10 +112,11 @@ while space_exist():
     #
     if won:
         # TODO
+        result = ' Congrats! You wom !'
 
         break
-    elif computer_move()[1]:
-        result = '=== You lose ! =='
+result = 'You lose !'        
+result = '=== You lose ! =='
         break
 print_board()
 print(result)
